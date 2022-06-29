004b0eed9957d9367e85a20a1e490a854b4dac99
domingo1021 <domingo1021>
2022-06-29 15:58:48 +0800

    edit two sum to O(n)

--- Week-3/Assignments/two_sum.js
+++ Week-3/Assignments/two_sum.js
@@ -3,17 +3,17 @@ function twoSum(nums, target) {
   let record = {};
   for (let i = 0; i < nums.length; i++) {
     let difference = target - nums[i];
-    if (Object.keys(record).includes(nums[i].toString())) {
+    if (record[nums[i].toString()] !== undefined) {
       return [record[`${nums[i]}`], i];
     }
     record[`${difference}`] = i;
   }
   return -1;
 }
-console.log(twoSum([2, 7, 11, 15], 9));
-console.log(twoSum([2, 7, 11, 15], 26));
-console.log(twoSum([1, 1, 1, 1], 2));
-console.log(twoSum([-1], 10));
+console.log(twoSum([2, 7, 11, 15], 9)); //[0, 1]
+console.log(twoSum([2, 7, 11, 15], 26)); //[2, 3]
+console.log(twoSum([1, 1, 1, 1], 2)); //[0, 1]
+console.log(twoSum([-1], 10)); // -1
 
 /*
 For example:
